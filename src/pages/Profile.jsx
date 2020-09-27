ff98f337d7be97f24afb8af8cf26712adc742dea
Santiago Henao <51752344+santiagohenao97@users.noreply.github.com>
2020-09-27 13:12:29 -0500

    Initial commit

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export default class Profile extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hola, aquí va mi perfil </h1>
+      </>
+    );
+  }
+}
